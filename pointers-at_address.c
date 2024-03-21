804e0bdbfc5d16bd7b35d064b81d2e83286cc2ab
Dustyn <dustyn_gibb@hotmail.com>
2024-03-21 19:42:41 -0400

    new file added

---
+++ pointers-at_address.c
@@ -0,0 +1,23 @@
+//
+// Created by dustyn on 3/21/24.
+//
+#include <stdio.h>
+int main(void)
+{
+    //this allocates a memory address to store the integer 10
+    int x = 10;
+
+    //however if you try to, as you would in a higher language, assign the value of x to the pointyer, you are just duplicating the value in memory
+    //You would want to address the specific memory address in which the value of x is located, you do this with address-of operatorp= &x;
+    int *pointer = &x; // = x;
+
+
+    //You can utilize pointers to other pointers as well which can be useful in some situations
+    int **point2point = &pointer;
+    **point2point = 20;
+
+    //notice when we set our 2nd pointer to 20, since it points to the address of pointer which points ot the address of x, this changes the value of x.
+    printf("%d\n",x);
+
+
+}
