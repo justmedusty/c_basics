8f1e06f9d568cdc762ccbefc978d38ee8cc03470
Dustyn <dustyn_gibb@hotmail.com>
2024-03-22 09:21:53 -0400

    comment fixing on why what was happening was happening

--- BitwiseOperations.c
+++ BitwiseOperations.c
@@ -9,7 +9,7 @@ int main(void)
     const unsigned int a = 0b1010; // 10 as binary
     const unsigned int b = 0b1111; //15 in binary
 
-    //this is a bitwise AND which will compare each bit and return 1 if the same, 0 if not. This should leav us with 1010(10 in binary)
+    //this is a bitwise AND which will compare each bit and return 1 if the same, 0 if not. This should leave us with 1010(10 in binary)
     const unsigned int c = a & b;
     printf("%d\n",c);
 
@@ -26,22 +26,23 @@ int main(void)
     printf("%d\n",f);
 
     //another thing we can do is bitwise OR which, as I'm sure you can guess, will compare each bit and return 1 if any is 1 or 0 if they are both 0
-    //this will result in 1111, 15 , the value of b, since no matter what one bit will be 1 dude to all 4 bits having a 1
+    //this will result in 1111, 15 , the value of b, since no matter what one bit will be 1 due to all 4 bits having a 1
     const int g = a | b;
     printf("%d\n",g);
 
-    //we can also usse bitchwise XOR which will return 1 if both bits are different, 0 if they are the same
+    //we can also use bitchwise XOR which will return 1 if both bits are different, 0 if they are the same
     // this will result in 0101,(5)
     const int h = a ^ b;
     printf("%d\n",h);
 
     //lastly we have the NOT which will flip each bit to 0 from 1 and to 1 from 0
     // this will result in 0101 again (5)
-    // but it doesn't! it's -11. Which means that although I am delcaring unsigned inegers, they are being interpretted
-    //as signed and using 2s compliment. This means there are 4 trailing 0s which are flipped, flipping the MSB turning the number
-    //negative
+    // but it doesn't! it's -11. This is because the space allocated for our 4 bit integer was actually 32 bits, 4 bytes.
+    //the not operator flips every bit that you did not realize was there making the number exactly max ui32 - 11 (11 being the 2s compliment
+    // interpretation of the negative value if we represented it as a signed integer.
     const unsigned int i = ~a;
-    printf("%d\n",i);
+    printf("%u\n",i);
+
 
 
 
