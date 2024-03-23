269bebe3cf2702586d4d614489106288d0bba826
Dustyn <dustyn_gibb@hotmail.com>
2024-03-23 13:01:53 -0400

    added basic pointer arithmetic

---
+++ PointerArithmetic.c
@@ -0,0 +1,27 @@
+//
+// Created by dustyn on 3/23/24.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    const int array[] = {1,2,3,4,5,6,7,8,9,10};
+    const int *ptr = array;
+
+    for (int i =0;i<10;i++)
+    {
+        printf("Element : %d: %d\n",i,*ptr);
+        ptr++;
+    }
+
+    //reset pointer to point to first element in array
+    ptr = array;
+    printf("We have now reset the pointer to the first element:%d\n",*ptr);
+
+    //using pointer arithmetic to do the same thing
+    for(int i = 0;i<10;i++)
+    {
+        printf("Element : %d value : %d\n",i,*(ptr + i));
+    }
+}
