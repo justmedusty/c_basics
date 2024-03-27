7f7b3dcf29ce16f109a57614407703294076ab56
dustyn <dustyn_gibb@hotmail.com>
2024-03-27 05:09:17 -0400

    Added EndiannessConversion.c

---
+++ EndiannessConversion.c
@@ -0,0 +1,33 @@
+//
+// Created by dustyn on 3/27/24.
+//
+
+#include <stdio.h>
+
+//Endianness refers to where the MSB (most significant bit) is stored in a system. Generally when we read binary right to left, with the MSB being on the left,
+//is little endian, and big endian would be the MSB being on the right side
+//Don't understand? No problem
+// take this binary byte : 0000 0001 this is one in a small endian system
+//However representing this in a big endian system it would be represented like this : 1000 0000
+//It is flipped! Our original byte , 0000 0001 would be either 127 or 256 on a big endian system depending on whether this integer is signed or not
+int main(){
+    //We will start with our little endian byte
+    unsigned int littleEndian;
+    unsigned int bigEndian = 0b00000000;
+    //the number 213 in little endian format
+    littleEndian = 0b11010101;
+
+    //Now let's say we are transferring this data to a big endian system, the order of these bits need to be reversed!
+    //How exactly do we do that ?
+    //We can use the bitshift operator to accomplish this!
+
+    for(int i = 0;i < 8;i++){
+        //Confusing right? What we are doing here involves 4 bitwise operations. We need our little endian number (0b11010101) to be big endian (0xb10101011) Reversing the order of the bits
+        //We are using a bitwise OR, a leftward bitshift a bitwise AND , and a rightward bitshift.
+        //What are doing is extracting each bit starting from the small endian LSB (least significant bit) and moving it to the other side which is where the LSB is in a big endian byte.
+        bigEndian |= ((littleEndian >> i)&1) << (7 -i);
+    }
+    //This should print 171 if correct (0xb10101011)
+    printf("%d\n",bigEndian);
+    //EH VOILA ! 171 ! would you look at that, we have now just converted a small endian byte to a big endian byte ! This can be very useful for transferring data from a small endian to a big endian system!
+}
--- FetchUnixStreamSocketAddress.c
+++ FetchUnixStreamSocketAddress.c
@@ -9,17 +9,17 @@
 #include <arpa/inet.h>
 
 
-int main(){
+int streamSocketAddressInfo() {
 
     //We will request our internet protocol address information from the kernel, we will do this by
     //having both hints, and serverInfo for the results of our kernel call.
-    int status ;
+    int status;
     //We will fill this struct to pass to getaddrinfo()
     struct addrinfo hints;
     //This will point to the results of asking the kernel for information on the socket that we are looking for
     struct addrinfo *serverInfo;
     //Set memory to 0
-    memset(&hints,0,sizeof(hints));
+    memset(&hints, 0, sizeof(hints));
     //Address unspecified so can be either ipv4 OR ipv6
     hints.ai_family = AF_UNSPEC;
     //Socket type of stream which are tcp sockets. Datagram sockets are UDP sockets.
@@ -28,34 +28,36 @@ int main(){
     hints.ai_flags = AI_PASSIVE;
 
     //We will run getaddr info with no hostname, port 6969, pass hints as input, and receiver serverInfo as output
-    if((status = getaddrinfo(NULL,"6969",&hints,&serverInfo))!= 0){
+    if ((status = getaddrinfo(NULL, "6969", &hints, &serverInfo)) != 0) {
         //If non-zero return value, error occurred and use gai_strerror(status) to print the status
-        fprintf(stderr,"getaddrinfo error %s\n", gai_strerror(status));
+        fprintf(stderr, "getaddrinfo error %s\n", gai_strerror(status));
         exit(EXIT_FAILURE);
     }
 
     //Iterate through the list by dereferencing the pointer to the next entry in the list
-    for(int i = 0;serverInfo != NULL; i++) {
+    for (int i = 0; serverInfo != NULL; i++) {
         //We can check the ip addresses available , we will dereference the serverInfo pointer to access the memory where cannon name is stored, and then do the same for dereferencing the ai addr field of serverInfo and again dereferencing to access the socket
         //address data stored in sa_data of the sockaddr struct as defined in socket.h header file.
 
-        printf("This is entry %d in the list \n",i);
+        printf("This is entry %d in the list \n", i);
 
         //Is this an IPv6 Address?
-        if (serverInfo->ai_family == AF_INET6){
+        if (serverInfo->ai_family == AF_INET6) {
             char ipaddr6[INET6_ADDRSTRLEN];
             //If so , convert this network representation to presentation which will be readable and not binary. We do this by declaring this is IPv6(AF_INET6), dereferencing the ai_addr sockaddr struct and then again dereferencing the socket address data field
             //we then assign this so the ipaddr6 chararray of size INET6_ADDRSTRLEN (the length of an ipv6 address
-            inet_ntop(AF_INET6,&serverInfo->ai_addr->sa_data,ipaddr6,INET6_ADDRSTRLEN);
-            printf("The IPv6 Address is %s\n",ipaddr6);
+            inet_ntop(AF_INET6, &serverInfo->ai_addr->sa_data, ipaddr6, INET6_ADDRSTRLEN);
+            printf("The IPv6 Address is %s\n", ipaddr6);
             //Is this an IPv4 address?
-        }else if (serverInfo->ai_family == AF_INET){
+
+        } else if (serverInfo->ai_family == AF_INET) {
             char ipaddr[INET_ADDRSTRLEN];
             //Again ,convert from network presentation to normal presentation, we do not want a binary address. Dereference the address information pointer that points to the socket address field of the sockaddr struct
-            //Assign to ipaddr which is a char of size INET_ADDRSTRLEN ( the size of the an ipv4 address
-            inet_ntop(AF_INET,&serverInfo->ai_addr->sa_data,ipaddr,INET_ADDRSTRLEN);
-            printf("The IPv4 Address is %s\n",ipaddr);
+            //Assign to ipaddr which is a char of size INET_ADDRSTRLEN ( the size of an ipv4 address
+            inet_ntop(AF_INET, &serverInfo->ai_addr->sa_data, ipaddr, INET_ADDRSTRLEN);
+            printf("The IPv4 Address is %s\n", ipaddr);
         }
+
         //Iterate through the linked list by referencing our result struct and setting it to the pointer of the next entry in the list
         serverInfo = serverInfo->ai_next;
 
