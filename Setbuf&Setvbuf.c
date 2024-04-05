6061f3b43b81dbf1ec31792a940a2d362072f7ff
dustyn <dustyn_gibb@hotmail.com>
2024-04-05 05:30:46 -0400

    clarifications

--- Setbuf&Setvbuf.c
+++ Setbuf&Setvbuf.c
@@ -6,6 +6,9 @@
  * In C our streams are almost always buffered, there are 3 types of buffering. Full buffering, line buffering, and no buffering. Full buffering means I/O does not happen until the buffer is full, line buffering
  * means that the new line character will cause the buffer to be flushed, and no buffering means direct I/O. This can be manipulated in C using setbuf or setvbuf. setvf buf has some bitmask flags which can be used to modify
  * a steams buffer behavior. With setbuf we can pass our own buffer , this generally will need to be of size BUFSIZ which is a predefined constant
+ *
+ * Note: Unbuffered streams are generally not wanted except in more specific situations since it will call a read and write for every single character that enters the stream which as you can imagine,
+ * is not the most performant in a lot of common scenarios.
  */
 
 #include <stdio.h>
@@ -20,8 +23,8 @@ int setbuf_and_setvbuf(){
     char buffer[BUFSIZ];
 
     //We will change the buffering behaviour of the stdout stream because why not, that is fun
-    //With this invocation, stdout will not be fully buffered using our own buffer, it is fully buffered by default
-    //except in a terminal environment where it is line buffered
+    //With this invocation, stdout will now be fully buffered using our own buffer, it is fully buffered by default
+    //except in a terminal environment where it is line buffered by default
     setbuf(stdout,buffer);
 
     //We can use setbuf to turn buffering off entirely too! Just pass NULL for the buffer
@@ -49,7 +52,7 @@ int setbuf_and_setvbuf(){
 
     /*
      * It is more laborious for the kernel when we use unbuffered streams however they have their place, and you can see a difference here when we use printf which goes through the fully buffered stdout stream,
-     * anything that does not flush your strems automatically you would not see the print message and would not manually flush it yourself!
+     * anything that does not flush your streams automatically you would not see the print message and would not manually flush it yourself!
      */
 
 
