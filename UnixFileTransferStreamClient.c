1bb5b9767f93da3881feb3f564d2b471340e0c21
dustyn <dustyn_gibb@hotmail.com>
2024-04-21 12:50:03 -0400

    Fixed and added comments to UnixFileTransferStreamClient.c and UnixFileTransferStreamServer.c

--- UnixFileTransferStreamClient.c
+++ UnixFileTransferStreamClient.c
@@ -14,15 +14,25 @@
 #include "sys/stat.h"
 
 #define BUFFSIZE 4096
-int main1(int argc, char *argv[]) {
+
+/*
+ * This is the client program for our file transfer client/server over TCP.I will not comment the regular socket init and connection systemc c since I already commented this extensively in other files.
+ * I will just comment the necessary file transfer portion.
+ *
+ * I am assuming you understand the basics of socket programming before you read this.
+ */
+
+int main2(int argc, char *argv[]) {
 
     if (argc < 3) {
         fprintf(stderr, "Usage: ftcp server_ip file_path");
     }
+
     int sock_fd;
     struct addrinfo hints, *pointer, *server;
     int return_value;
     char buffer[BUFFSIZE];
+    int yes = 1;
 
 
     memset(&hints, 0, sizeof hints);
@@ -37,9 +47,10 @@ int main1(int argc, char *argv[]) {
 
     for (pointer = server; pointer != NULL; pointer = pointer->ai_next) {
         if ((sock_fd = socket(pointer->ai_family, pointer->ai_socktype, pointer->ai_protocol)) == -1) {
-            perror("Couldnt make socket and get file descriptor from kernel\n");
+            perror("Couldn't make socket and get file descriptor from kernel\n");
             continue;
         }
+
 
         if (connect(sock_fd, pointer->ai_addr, pointer->ai_addrlen) == -1) {
             close(sock_fd);
@@ -57,6 +68,17 @@ int main1(int argc, char *argv[]) {
 
     freeaddrinfo(server);
 
+
+    /*
+     * We are going to open the file at path specified by the 3rd arg to this program.
+     * We will open with fopen using the flag "rb" which means read binary.
+     * After which we will init a stat struct which we we will call stat() on to fill the struct
+     * with information about that file with information from the i-node table. We will use this to veryify
+     * the amount of bytes being sent.
+     *
+     * We will loop on fread filling our buffer until we reach EOF.
+     *
+     */
     FILE *file = fopen(argv[2], "rb");
     if (file == NULL) {
         perror("Error opening file");
@@ -69,27 +91,46 @@ int main1(int argc, char *argv[]) {
     size_t bytes_read;
     struct stat stat1;
 
+
     if(stat(argv[2],&stat1) == -1){
         perror("stat");
         exit(EXIT_FAILURE);
     }
-    size_t byte_total = 0;
-    while ((bytes_read = fread(buffer, sizeof(char), BUFFSIZE, file)) > 0) {
 
+    size_t byte_total = 0;
+
+    /*
+     * We will loop on fread until we reach end of file, we will read sizeof char * buffsize, since of char is always 1 byte so we will read BUFFSIZE bytes, chunking out
+     * the file reading to keep things clean. As we read into the buffer we'll fire off binary data out to the server over the open connection we have on our socket.
+     * Iterate byte total after each iteration.
+     */
+    while ((bytes_read = fread(buffer, sizeof(char), BUFFSIZE, file)) > 0) {
+        if(byte_total == stat1.st_size){
+            printf("Full file sent\n");
+            break;
+        }
         ssize_t bytes_sent = send(sock_fd, buffer, bytes_read, 0);
         if (bytes_sent == -1) {
-            perror("Error sending data over socket");
+            perror("send");
             exit(EXIT_FAILURE);
         }
         byte_total += bytes_read;
     }
+
+    /*
+     * Here we will do a check on the size of the bytes sent, make sure we sent all of it
+     */
     if(byte_total >= stat1.st_size) {
-        printf("file sent\n");
+        printf("bytes sent : %lo bytes total in file %lo\n",byte_total,stat1.st_size);
+
     }
     if (ferror(file)) {
         perror("Error reading file");
         exit(EXIT_FAILURE);
     }
+    /*
+     * That's it ! close the socket fd and the file!
+     */
 
     close(sock_fd);
 
--- UnixFileTransferStreamServer.c
+++ UnixFileTransferStreamServer.c
@@ -2,6 +2,7 @@
 // Created by dustyn on 4/20/24.
 //
 #include <stdio.h>
+#include <sys/wait.h>
 #include "sys/socket.h"
 #include "netdb.h"
 #include "unistd.h"
@@ -14,52 +15,58 @@
 #define BACKLOG 15
 #define MAX_PATH_SIZE 100
 #define BUFFSIZE 4096
-#define DOWNLOAD_FOLDER "/home/dustyn/Downloads"
-/*
- * I am not going to comment any of the code that is very similar to other code I have used so far in other files since this is focusing on file transfer. If you are confused about any uncommented code here, go look at some of my other files to read
- * what each line and system call is doing. I am just going to write it out and not comment until we are ready for some file transfer
- *
- * Go look at my other unix socket programming files for intro stuff. I am assuming you know what you are looking at here.
- */
-void* get_internet_address(struct sockaddr *sockad){
 
-    if(sockad->sa_family == AF_INET){
-        return &(((struct sockaddr_in *) sockad)->sin_addr);
-    }
-    else {
-        return &((struct sockaddr_in6 *) sockad)->sin6_addr;
+/*
+ * I am not going to comment any of the code that is very similar and/or identical to other code I have used so far in other files since this is focusing on file transfer.
+ * If you are confused about any uncommented code here, go look at some of my other files to read
+ * what each line and system call is doing. I am just going to write it out and not comment until we are ready for some file transfers
+ *
+ * Go look at my other unix socket programming files for intro stuff.
+ *
+ *
+ *
+ * I am assuming you know what you are looking at here.
+ */
+
+void *get_internet_address(struct sockaddr *sock_addr) {
+
+    if (sock_addr->sa_family == AF_INET) {
+        return &(((struct sockaddr_in *) sock_addr)->sin_addr);
+    } else {
+        return &((struct sockaddr_in6 *) sock_addr)->sin6_addr;
     }
 
 }
-int get_listener_socket(void){
+
+int get_listener_socket(void) {
 
     struct addrinfo hints, *results, *pointer;
     int listener_fd;
     int addr_ret;
-    memset(&hints,0,sizeof hints);
+    memset(&hints, 0, sizeof hints);
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_family = AF_UNSPEC;
     hints.ai_flags = AI_PASSIVE;
     int yes = 1;
 
-    addr_ret = getaddrinfo(NULL,PORT,&hints,&results);
+    addr_ret = getaddrinfo(NULL, PORT, &hints, &results);
 
-    if(addr_ret < 0){
+    if (addr_ret < 0) {
         gai_strerror(addr_ret);
         exit(EXIT_FAILURE);
     }
 
-    for(pointer = results;pointer!= NULL; pointer = pointer->ai_next ){
+    for (pointer = results; pointer != NULL; pointer = pointer->ai_next) {
 
-        listener_fd = socket(pointer->ai_family,pointer->ai_socktype,pointer->ai_protocol);
-        if(listener_fd < 0){
+        listener_fd = socket(pointer->ai_family, pointer->ai_socktype, pointer->ai_protocol);
+        if (listener_fd < 0) {
             perror("socket");
             continue;
         }
 
-        setsockopt(listener_fd,SOL_SOCKET,SO_REUSEADDR, &yes ,sizeof (int));
+        setsockopt(listener_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
-        if (bind(listener_fd, pointer->ai_addr,pointer->ai_addrlen) < 0){
+        if (bind(listener_fd, pointer->ai_addr, pointer->ai_addrlen) < 0) {
             perror("bind");
             close(listener_fd);
             continue;
@@ -73,147 +80,146 @@ int get_listener_socket(void){
         exit(EXIT_FAILURE);
     }
 
-    if(listen(listener_fd,BACKLOG) < 0){
+    if (listen(listener_fd, BACKLOG) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
     char serverIp[INET6_ADDRSTRLEN];
-    inet_ntop(pointer->ai_addr->sa_family, get_internet_address((struct sockaddr *) &pointer->ai_addr), serverIp, INET6_ADDRSTRLEN);
-    printf("Server IP is %s\n",serverIp);
+    inet_ntop(pointer->ai_addr->sa_family, get_internet_address((struct sockaddr *) &pointer->ai_addr), serverIp,
+              INET6_ADDRSTRLEN);
+    printf("Server IP is %s\n", serverIp);
 
     return listener_fd;
 
 }
-void add_to_poll_fds(struct pollfd *pollfds[], int newFd ,int *fd_count,int *fd_size){
-
-
-    if(fd_size == fd_count){
-        *fd_size *= 2;
-        *pollfds = realloc(*pollfds, (sizeof (struct pollfd**)) * *fd_size);
-    }
-
-    (*pollfds)[*fd_count].fd = newFd;
-    (*pollfds)[*fd_count].events = POLLIN;
-
-    *fd_count += 2;
-
-}
-
-void del_from_poll_fds(struct pollfd *pollfds[], int index, int *fd_count){
-    pollfds[index] = pollfds[*fd_count - 1];
-    (*fd_count)--;
-
-}
-
-
-int main(){
-    int fd_count;
-    int fd_size = 5;
-    int listener;
-    struct pollfd *pollfds = malloc(sizeof (*pollfds) * fd_size);
-    int new_fd;
-    char path[MAX_PATH_SIZE];
-    struct sockaddr_storage client_address;
-    socklen_t client_addr_len;
-    char buffer[BUFFSIZE];
-    size_t bytes;
-    char client_ip[INET6_ADDRSTRLEN];
-
-
-    if(pollfds == NULL){
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-
-    listener = get_listener_socket();
-    if(listener < 0){
-        fprintf(stderr,"Error getting listener socket\n");
-        exit(EXIT_FAILURE);
-    }
-    pollfds[0].fd = listener;
-    pollfds[0].events = POLLIN;
-    fd_count =1;
-    printf("Server listener online\n");
-
-    for(;;){
-        int poll_count = poll(pollfds,fd_count, -1);
-
-        if(poll_count < 0){
-            perror("poll");
-            exit(EXIT_FAILURE);
-        }
-
-        for (int (i) = 0; (i) < fd_count; ++(i)) {
-
-            if(pollfds[i].revents == POLLIN){
-                client_addr_len = sizeof client_address;
-                if(pollfds[i].fd == listener){
-                    new_fd = accept(listener,(struct sockaddr* )&client_address,&client_addr_len);
-                    if(new_fd < 0){
-                        perror("accept");
-                        close(new_fd);
-                        continue;
-                    }
-
-                    inet_ntop(client_address.ss_family,get_internet_address((struct sockaddr *) &client_address), client_ip, INET6_ADDRSTRLEN);
-
-                    printf("New connection on socket %d from address %s\n",new_fd, client_ip);
-
-                    add_to_poll_fds(&pollfds,new_fd,&fd_count,&fd_size);
-                    strcpy(buffer,"You are now connected to the file transfer server\n");
-                    if((bytes = send(new_fd,&buffer,sizeof buffer,0)) < 0){
-                        perror("send");
-                        continue;
-                    }
 
 /*
  * So instead of the usual echoing a message, we can use fwrite with an open file with the wb mode (write binary) to write everything we are receiving into a file of
  * the specified name. For this, I will keep it simple and just have 1 hardcoded location obviously if we have many connections we will need to take arguments from clients.
  * This is just an elementary example to show how a file can be transferred over TCP.
  *
- * The client will allow
+ * The client specifies whatever file they want to send, and the server will write the binary data to a hardcoded file path. Later on we can make this a little more fleshed out feature wise,
+ * where the client can specify what file they want and fire that data over, and we will write it.
+ * This is working by both the client and server looping on send() and recv() respectively until the end of the file is reached for the client and the end of stream for the server. From this point the file has been
+ * transferred successfully, and we can close everything up and close the file descriptor
+ *
+ * The flag "wb" in the fopen mode means write binary, in the client we will be using the flag "rb", read binary. Then we will just fill our buffer and fire it all off over to the server which will then write it to a predetermined file path.
+ *
+ *
+ *
  */
-                } else{
 
-                    FILE *file = fopen("/home/dustyn/file.txt", "wb");
-                    if (file == NULL) {
-                        perror("fopen");
-                        // Handle error
-                    }
 
-                    while ((bytes = recv(pollfds[i].fd, buffer, BUFFSIZE, 0)) > 0) {
-                        size_t bytes_written = fwrite(buffer, sizeof(char), bytes, file);
-                        if (bytes_written != bytes) {
-                            perror("fwrite");
-                        }
-                    }
-                    perror("recv");
-                    perror("fwrite");
+void handle_file_transfer(int sock_fd, char *buffer[]) {
 
-                    if (bytes == -1) {
-                        perror("recv");
+    ssize_t bytes = 0;
 
-                    }
+    FILE *file = fopen("/home/dustyn/file.txt", "wb");
 
-                    if (bytes == 0) {
-                        printf("Socket %d disconnected\n", pollfds[i].fd);
-                        close(pollfds[i].fd);
-                        del_from_poll_fds(&pollfds, i, &fd_count);
-                        fclose(file);
-                        break;
-                    }
+    if (file == NULL) {
+        perror("fopen");
+        // Handle error
+    }
 
+    while ((bytes = recv(sock_fd, buffer, BUFFSIZE, 0)) > 0) {
+        size_t bytes_written = fwrite(buffer, sizeof(char), bytes, file);
+        if (bytes_written != bytes) {
+            perror("fwrite");
+        }
+    }
+
+    if (bytes == -1) {
+        perror("recv");
+
+    }
+
+    //The client has hung up, print a message, close the socket, close the file
+    if (bytes == 0) {
+        printf("Socket %d disconnected\n", sock_fd);
+        close(sock_fd);
+        fclose(file);
+    }
+
+    return;
+
+}
+
+int main() {
+    int fd_count;
+    int fd_size = 1;
+    int listener;
+    struct pollfd *pollfds = malloc(sizeof(*pollfds) * fd_size);
+    int new_fd;
+    struct sockaddr_storage client_address;
+    socklen_t client_addr_len;
+    char buffer[BUFFSIZE];
+    char client_ip[INET6_ADDRSTRLEN];
+
+
+    if (pollfds == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    listener = get_listener_socket();
+    if (listener < 0) {
+        fprintf(stderr, "Error getting listener socket\n");
+        exit(EXIT_FAILURE);
+    }
+    pollfds[0].fd = listener;
+    pollfds[0].events = POLLIN;
+    fd_count = 1;
+    printf("Server listener online\n");
+
+    for (;;) {
+        int poll_count = poll(pollfds, fd_count, -1);
+
+        if (poll_count < 0) {
+            perror("poll");
+            exit(EXIT_FAILURE);
+        }
+
+        for (int (i) = 0; (i) < fd_count; ++(i)) {
+
+            if (pollfds[i].revents == POLLIN) {
+                client_addr_len = sizeof client_address;
+                new_fd = accept(listener, (struct sockaddr *) &client_address, &client_addr_len);
+                if (new_fd < 0) {
+                    perror("accept");
+                    close(new_fd);
+                    continue;
                 }
+
+                inet_ntop(client_address.ss_family, get_internet_address((struct sockaddr *) &client_address),client_ip, INET6_ADDRSTRLEN);
+
+                printf("New connection on socket %d from address %s\n", new_fd, client_ip);
+
+                /*
+                 * On each connection we're going to immediately fork and call our file transfer handler server function
+                 * defined above. We will wait for the client to finish before
+                 */
+                pid_t child = fork();
+
+                if (fork < 0) {
+                    perror("fork");
+                    exit(EXIT_FAILURE);
+                }
+
+                if (child == 0) {
+                    handle_file_transfer(new_fd, buffer);
+                    exit(EXIT_SUCCESS);
+                } else {
+                    close(new_fd);
+                    wait(NULL);
+                }
+
+
             }
 
         }
     }
 
 
-
-
-
-
-
 }
+
